990d3f11f7db99edb180ce708cd9cd6e13866966
Joe Schlachtenhaufen <JDSchlach@gmail.com>
2019-05-09 11:42:32 -0500

    created components for pet input and owner inputs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import AddOwner from '../AddOwner/AddOwner';
+import AddPet from '../AddPet/AddPet';
+import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Pet Hotel</h1>
-      </div>
+      <Router>
+        <div>
+          <header className="MainHead">
+            <h1>PET HOTEL</h1>
+          </header>
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route
+              exact
+              path="/home"
+              component={AddPet}
+            />
+            <Route
+              exact
+              path="/owner"
+              component={AddOwner}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
